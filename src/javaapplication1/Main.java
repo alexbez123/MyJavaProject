009ddbfd8b675cfafe8c631650d32157eb2f84ee
AlexBez <a.bezouglov@gmail.com>
2014-04-03 10:56:49 -0400

    Added git ignore file and changed java files

--- src/javaapplication1/Main.java
+++ src/javaapplication1/Main.java
@@ -8,8 +8,11 @@ public class Main {
     public static void main(String[] args)
     {
 
+        int x= 5;
+        int y= 10;
+
        System.out.println("This is a heroku test");
-       System.out.println("Hello world");
+       System.out.println("Hello world"+(x+y));
 
     }
 
